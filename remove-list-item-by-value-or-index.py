a43a350896d4921fb61c90da6efdb3ed976b7311
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 12:36:31 +0200

    Create remove-list-item-by-value-or-index.py

---
+++ remove-list-item-by-value-or-index.py
@@ -0,0 +1,11 @@
+#lista z imionami, niech wyswietli je bez 2giego
+
+list=['Anna','Beata', 'Karolina','Lala', 'Bartek']
+
+# remove list value:
+list.remove('Beata')
+print(list)
+
+#   OR remove list index:
+del list[-1]
+print(list)
